d269adba4264d052aed504144a5cdc9364fb8e50
siddheshkothadi <realsiddheshko@gmail.com>
2021-10-27 22:57:51 +0530

    added timer code

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,36 @@ export default function Home() {
   const [words, setWords] = React.useState("");
   const [typedWords, setTypedWords] = React.useState("");
 
+  const [chosenLanguage, setChosenLanguage] = React.useState("js");
+  const [toggle, setToggle] = React.useState(false);
+  const [isFocussed, setIsFocussed] = React.useState(false);
+  const [timer, setTimer] = React.useState(0);
+  const [intervalState, setIntervalState] = React.useState(null);
+
+  const startTimer = () => {
+    setTimer(0);
+    const interval = setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    clearInterval(intervalState);
+    setIntervalState(null);
+    setTimer(0);
+  };
+
   React.useEffect(() => {
+    window.document.getElementById("type-box")?.addEventListener("blur", () => {
+      setIsFocussed(false);
+    });
+    window.document
+      .getElementById("type-box")
+      ?.addEventListener("focus", () => {
+        setIsFocussed(true);
+      });
     const res = fetch(
-      "https://siddheshkothadi.github.io/APIData/language-keywords/js.json"
+      `https://siddheshkothadi.github.io/APIData/language-keywords/${chosenLanguage}.json`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -23,7 +50,23 @@ export default function Home() {
         }
         setWords(fiftyWords.join(" "));
       });
-  }, []);
+
+    return () => {
+      // remove event listeners
+    };
+  }, [toggle]);
+
+  React.useEffect(() => {
+    if (typedWords.length == 1) {
+      startTimer();
+    } else if (typedWords.split(" ").length > 25) {
+      stopTimer();
+    }
+  }, [typedWords]);
+
+  React.useEffect(() => {
+    console.log("focus state", isFocussed);
+  }, [isFocussed]);
 
   if (words.length === 0) {
     return <div>Loading...</div>;
@@ -32,6 +75,7 @@ export default function Home() {
   return (
     <div className="bg-drBackground flex justify-center min-h-screen min-w-screen">
       <div className="max-w-7xl w-screen h-screen flex p-6 items-center justify-center flex-wrap flex-col">
+        <p className="text-drForeGround my-2 text-lg">{timer}</p>
         <div
           className="flex flex-wrap noSelect"
           onClick={() => {
@@ -56,6 +100,17 @@ export default function Home() {
           value={typedWords}
           autoFocus
         />
+        <button
+          className="text-drForeGround"
+          onClick={() => {
+            stopTimer();
+            setTypedWords("");
+            setToggle(!toggle);
+            window.document.getElementById("type-box").focus();
+          }}
+        >
+          Refresh
+        </button>
       </div>
     </div>
   );
