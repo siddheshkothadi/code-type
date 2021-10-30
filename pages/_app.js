0bc5058133e720321192f794494b683f6c5bc50b
siddheshkothadi <realsiddheshko@gmail.com>
2021-10-30 09:17:15 +0530

    added screenshot

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-// import "tailwindcss/tailwind.css";
-import "../styles/global.css";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
